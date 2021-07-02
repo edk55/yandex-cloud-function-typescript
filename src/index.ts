269e89d49c25131b4187fd300d86717f3af0b3bd
Eugene Korobkov <e@korobkov.io>
2021-07-02 19:46:15 +0300

    Added console.log to test CI/CD pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Handler } from './typings';
 
 export const handler: Handler = (event, context) => {
+  console.log('Hello world!');
+
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context }),
